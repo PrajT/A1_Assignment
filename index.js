d2d8e76add9d5ad26166e2bedcddbe3b05b79d9c
PrajT <105973365+PrajT@users.noreply.github.com>
2024-06-18 14:29:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("A1_Assignment");
+console.log("---- ---- ----");
+
+let totalEvents = 3;
+let eventOneTitle = "Music Concert";
+let eventOneDate = "August 15, 2023";
+let eventOneTime = "7:00 PM";
+let eventOneTicketsSold = 150;
+let eventOneTicketPrice = 2599;
+console.log("Total Events: " + totalEvents);
+
+console.log("---- ---- ----" + "\n");
+console.log("Event One:" + "\n");
+console.log("Title: " + eventOneTitle);
+console.log("Date: " + eventOneDate);
+console.log("Time: " + eventOneTime);
+console.log("Tickets Sold: " + eventOneTicketsSold);
+console.log("Ticket Price: " + eventOneTicketPrice);
+
+console.log("---- ---- ----" + "\n");
+let eventTwoTitle = "Art Exhibition";
+let eventTwoDate = "September 10, 2023";
+let eventTwoTime = "11:00 AM";
+let eventTwoTicketsSold = 80;
+let eventTwoTicketPrice = 159;
+console.log("Event Two:" + "\n");
+console.log("Title: " + eventTwoTitle);
+console.log("Date: " + eventTwoDate);
+console.log("Time: " + eventTwoTime);
+console.log("Tickets Sold: " + eventTwoTicketsSold);
+console.log("Ticket Price: " + eventTwoTicketPrice);
+
+console.log("---- ---- ----" + "\n");
+let eventThreeTitle = "Sports Tournament";
+let eventThreeDate = "October 5, 2023";
+let eventThreeTime = "2:30 PM";
+let eventThreeTicketsSold = 200;
+let eventThreeTicketPrice = 1299;
+console.log("Event Three:" + "\n");
+console.log("Title: " + eventThreeTitle);
+console.log("Date: " + eventThreeDate);
+console.log("Time: " + eventThreeTime);
+console.log("Tickets Sold: " + eventThreeTicketsSold);
+console.log("Ticket Price: " + eventThreeTicketPrice);
